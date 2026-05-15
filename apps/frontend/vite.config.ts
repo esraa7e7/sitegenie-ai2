723f3ec2d3edd6f8481b5b9bd2ee3699bcace855
esraa7e7 <esraabbas64@gmail.com>
2026-05-15 15:18:33 +0300

    s

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base: './', // لضمان عمل المسارات النسبية بشكل صحيح
 
   server: {
     port: 5173,
