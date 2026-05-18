42f323ddd91419c4cf5fcd788b253882f95aaf16
esraa7e7 <esraabbas64@gmail.com>
2026-05-18 22:52:23 +0300

    fix metadata

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -237,6 +237,7 @@ export function pick<T, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
   const result = {} as Pick<T, K>;
   keys.forEach((key) => {
     if (key in obj) {
+      // @ts-ignore
       result[key] = obj[key];
     }
   });
