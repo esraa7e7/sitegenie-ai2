6cacf41c004fed9c99f05dee617dfb9521a931b0
esraa7e7 <esraabbas64@gmail.com>
2026-05-18 23:05:34 +0300

    fix metadata

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -233,7 +233,7 @@ export function isEmpty(value: unknown): boolean {
 /**
  * Pick specific properties from object
  */
-export function pick<T, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
+export function pick<T extends object, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
   const result = {} as Pick<T, K>;
   keys.forEach((key) => {
     if (key in obj) {
